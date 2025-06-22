f0f81d5b7ca66c4136a3fb42162b672ec63968ac
DoctorReid <doctorreid2024@outlook.com>
2025-06-22 16:39:23 +0800

    perf: 闪光模型 迷失之地模型 更新

--- src/zzz_od/yolo/flash_classifier.py
+++ src/zzz_od/yolo/flash_classifier.py
@@ -7,16 +7,17 @@ from one_dragon.yolo.yolov8_onnx_cls import Yolov8Classifier
 
 class FlashClassifier(Yolov8Classifier):
 
-    def __init__(self,
-                 model_name: str = 'yolov8n-640-dodge-0718',
-                 backup_model_name: str = 'yolov8n-640-dodge-0718',
-                 model_parent_dir_path: Optional[str] = os.path.abspath(__file__),  # 默认使用本文件的目录
-                 gh_proxy: bool = True,
-                 gh_proxy_url: Optional[str] = None,
-                 personal_proxy: Optional[str] = None,
-                 gpu: bool = False,
-                 keep_result_seconds: float = 2
-                 ):
+    def __init__(
+            self,
+            model_name: str = 'yolov8n-640-flash-20250622',
+            backup_model_name: str = 'yolov8n-640-flash-20250622',
+            model_parent_dir_path: Optional[str] = os.path.abspath(__file__),  # 默认使用本文件的目录
+            gh_proxy: bool = True,
+            gh_proxy_url: Optional[str] = None,
+            personal_proxy: Optional[str] = None,
+            gpu: bool = False,
+            keep_result_seconds: float = 2
+    ):
         """
         :param model_name: 模型名称 在根目录下会有一个以模型名称创建的子文件夹
         :param model_parent_dir_path: 放置所有模型的根目录
@@ -35,3 +36,17 @@ class FlashClassifier(Yolov8Classifier):
             gpu=gpu,
             keep_result_seconds=keep_result_seconds
         )
+
+
+def __debug():
+    from one_dragon.utils import os_utils
+    flash_classifier = FlashClassifier(
+        model_parent_dir_path=os_utils.get_path_under_work_dir('assets', 'models', 'flash_classifier')
+    )
+    from one_dragon.utils import debug_utils
+    result = flash_classifier.run(debug_utils.get_debug_image('_1750517690304'))
+    print(result.class_idx)
+
+
+if __name__ == '__main__':
+    __debug()
